62f19c59bb10f8c6f740831a6591902092912fba
Tim <hello@timsmart.co>
2025-04-17 11:18:14 +1200

    fix .d.ts output

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -3,14 +3,15 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: ["src/*.ts"],
   format: ["esm", "cjs"],
+  tsconfig: "tsconfig.build.json",
   outExtension({ format }) {
     return {
-      js: format === "cjs" ? ".js" : ".mjs",
+      js: format === "cjs" ? ".js" : ".mjs"
     }
   },
   shims: true,
   splitting: true,
   sourcemap: true,
   clean: true,
-  treeshake: "smallest",
+  treeshake: "smallest"
 })
